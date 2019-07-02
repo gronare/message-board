2d167983ea2c252f2f722643a26e0a77445aab38
Carl Green <carl.green@gronare.se>
2019-07-02 19:54:51 +0200

    adds getter methods for email and password

--- app/commands/authenticate_user.rb
+++ app/commands/authenticate_user.rb
@@ -10,6 +10,10 @@ class AuthenticateUser
     JsonWebToken.encode(user_id: user.id) if user
   end
 
+  private
+
+  attr_accessor :email, :password
+
   def user
     user = User.find_by_email(email)
     return user if user && user.authenticate(password)
