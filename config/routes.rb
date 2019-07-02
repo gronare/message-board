9b3470b2d429d1a9c12a0e607308b962aca6ae42
Carl Green <carl.green@gronare.se>
2019-07-02 19:53:04 +0200

    updates routes to include restful paths for posting messages to message board

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  resources :posts, only: [:index, :create, :update, :destroy]
   post 'register', to: 'users#create'
   post 'authenticate', to: 'authentication#authenticate'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
